8ba50ef18d9c08cfed38ed52e1eae05079f77eb5
Rohit Ghosh <140050497+RohitGhosh07@users.noreply.github.com>
2025-07-22 14:01:16 +0530

    chore: remove docker-related files

--- docker/init.sql
+++
@@ -1,8 +0,0 @@
--- Connect to the database (it should already exist from POSTGRES_DB env var)
-\c bowlsacedb;
-
--- Create extensions if needed
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
--- Grant privileges
-GRANT ALL PRIVILEGES ON DATABASE bowlsacedb TO bowlsace;
